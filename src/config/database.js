3292350c9caad8fa759fdbc51c305dd53c8c4d62
faqihasyari <faqih8158@gmail.com>
2025-12-28 22:56:42 +0700

    add sequelize database

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+const {Sequelize} = require("sequelize");
+
+// ambil dari .env
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT || "mysql";
+
+// sequelize
+const sequelize = new Sequelize(
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    {
+        host: DB_HOST,
+        dialect: DB_DIALECT,
+        logging: false,
+        timezone: "+07:00",
+        dialectOptions: {
+            dateStrings: true,
+            typeCast: true,
+        }
+    }
+);
+
+module.exports = sequelize;
