43898f06c284ae24726a4bfa5d14c791383c2c76
faqihasyari <faqih8158@gmail.com>
2025-12-29 22:56:14 +0700

    add relasi permissions and model

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,11 @@
 const User = require("./User");
 const Attendance = require("./attendance");
+const Meetings = require("./meetings");
+const Permissions = require("./permissions");
 
 // menambahkan relasi
+
+// user ke permission
 User.hasMany(Attendance, {
     foreignKey: "user_id",
     onDelete: "CASCADE"
@@ -11,7 +15,24 @@ Attendance.belongsTo(User, {
     foreignKey: "user_id"
 });
 
+// meetings ke attendance
+Meetings.hasMany(Attendance, {
+    foreignKey: "meeting_id",
+    onDelete: "CASCADE"
+});
+
+// meetings ke permission
+Meetings.hasMany(Permissions, {
+    foreignKey: "meeting_id",
+    onDelete: "CASCADE"
+});
+Permissions.belongsTo(Meetings, {foreignKey : "meeting_id"});
+
+
+
 module.exports = {
     User,
-    Attendance
+    Meetings,
+    Attendance,
+    Permissions
 };
