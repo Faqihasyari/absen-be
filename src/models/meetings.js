6ad361bcf2588581b39426d211c5c343f595e2c0
faqihasyari <faqih8158@gmail.com>
2025-12-29 21:41:52 +0700

    add meetings model

---
+++ src/models/meetings.js
@@ -0,0 +1,21 @@
+const { DataTypes} = require("sequelize");
+const sequelize = require("../config/database");
+
+const Meetings = sequelize.define(
+    "meetings", 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nama_rapat:{
+             type: DataTypes.STRING,
+             allowNull: false
+        },
+        tanggal: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }
+)
--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const sequelize = require("./config/database");
+const app = require("./app");
+
+// import mode user
+require("./models/User")
+
+const PORT = process.env.PORT || 3000;
 
 (async () => {
     try {
         await sequelize.authenticate();
         console.log("Database berhasil tehubung");
+
+        await sequelize.sync({alter: true});
+        console.log("Table synced");
         process.exit(0);
+        // app.listen(PORT, () => {
+        //     console.log(`Server berjalan di port ${PORT}`);
+        // });
     } catch (error){
         console.error("Gagal terhubung ke database", error);
+        process.exit(1);
     }
 })();
