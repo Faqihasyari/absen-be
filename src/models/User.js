7b71ce66f76a1f88838adf5172e5e95fe3318f33
faqihasyari <faqih8158@gmail.com>
2025-12-29 21:07:34 +0700

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define(
+    "users",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nim: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        jabatan: {
+            type: DataTypes.STRING
+        },
+        role: {
+        type: DataTypes.ENUM("admin", "anggota"),
+        defaultValue: "anggota"
+        },
+        qr_token: {
+            type: DataTypes.STRING,
+            unique: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = User;
