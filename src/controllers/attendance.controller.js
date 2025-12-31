a7c26d1b82ffd7b90aa96d4cb3afcd94253e961f
faqihasyari <faqih8158@gmail.com>
2025-12-31 21:44:55 +0700

    add attendance controller

--- src/controllers/attendance.controller.js
+++ src/controllers/attendance.controller.js
@@ -1,1 +1,68 @@
-const {User, Meetings, Attendace} = require("../models");
+const {User, Meetings, Attendace, Attendance} = require("../models");
+
+exports.scanAttendance = async (req, res) => {
+    try {
+        const {qr_token, meeting_id, status} = req.body;
+
+        // mencari user berdasarkan qr token
+        const user = await User.findOne({
+            where: { qr_token }
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Scan QR gagal"
+            });
+        }
+
+        // ngecek meeting
+        const meeting = await Meetings.findByPk(meeting_id);
+        if (!meeting) {
+            return res.status(404).json({
+                message: "Rapat tidak ditemukan"
+            });
+        }
+
+        // cek status rapat
+        if (meeting.status !== "Sedang berlangsung"){
+            return res.status(400).json({
+                message: "Rapat sudah selesai, absensi telah ditutup!!"
+            });
+        }
+
+        // cek apakah user sudah absen apa belum
+        const existingAttendance = await Attendance.findOne({
+            where: {
+                user_id: user.id,
+                meeting_id: meeting.id
+            }
+        });
+
+        if (existingAttendance){
+            return res.status(400),json({
+                message: "User sudah melakukan absensi"
+            });
+        }
+
+        // menyimpan absensi
+        const attendance = await Attendance.create({
+            user_id: user.id,
+            meeting_id: meeting,
+             status: status || "alfa"
+        });
+
+        res.status(201).json({
+            message: "Absensi berhasil",
+            data: {
+                nama: user.nama,
+                rapat: meeting.nama_rapat,
+                status: attendance.status,
+                waktu_scan: attendance.waktu_scan
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+};
