2bb4ce44ee234c2155e64e2c0ad9c47061e417c5
faqihasyari <faqih8158@gmail.com>
2026-01-11 16:36:02 +0700

    delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { errorHandler } = require("../src/middlewares/errorHandler");
+const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("../src/routes"));
+app.use("/api", require("./routes"));
 
 app.use(errorHandler);
 
