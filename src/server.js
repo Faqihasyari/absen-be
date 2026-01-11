dc8a391341ec6f60466e5472723a090cc2b88fdc
faqihasyari <faqih8158@gmail.com>
2026-01-11 16:13:02 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const sequelize = require("./config/database");
-const app = require("./app");
+const app = require("../api/app");
 
 const PORT = process.env.PORT || 3000;
 
